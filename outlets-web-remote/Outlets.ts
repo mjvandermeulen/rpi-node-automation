8df8054eb7778b6e74f8877c863995c9d9338cd0
Maarten van der Meulen <mjvandermeulen@gmail.com>
2019-11-10 07:38:18 -0600

    moved outlets-web-remote project from adventures/w3schools to own folder. npm-ed dependencies

---
+++ outlets-web-remote/Outlets.ts
@@ -0,0 +1,106 @@
+import * as events from 'events'
+const eventEmitter = new events.EventEmitter()
+
+import * as socketio from 'socket.io'
+
+import { PythonShell, Options } from 'python-shell'
+
+interface SocketData {
+  group: string
+  value: number
+}
+
+export class Outlets {
+  private io
+  public socketName: string
+
+  private onOutlets
+  constructor(http, socketName) {
+    this.io = socketio(http) // require socket.io module
+    this.socketName = socketName
+    this.onOutlets = new Set([])
+    this.io.sockets.on('connection', socket => {
+      socket.on(this.socketName, (socketData: SocketData) => {
+        let currentValue = this.onOutlets.has(socketData.group) ? 1 : 0
+        console.log(`Server socket.on`)
+        console.log(`  currentValue: ${currentValue}`)
+        console.log(`  socketData.group: ${socketData.group}`)
+        console.log(`  socketData.value: ${socketData.value}`)
+
+        const returnData: SocketData = {
+          group: socketData.group,
+          value: currentValue,
+        }
+        if (socketData.value === -1) {
+          // light sync requested
+          // only emit to current socket.
+          returnData.value = currentValue
+          socket.emit(this.socketName, returnData)
+        } else {
+          // always send light signal, even if server thinks the modes match
+          this.switch(socketData.group, socketData.value === 1)
+          // emit to all sockets, except the current one
+          socket.broadcast.emit(this.socketName, socketData)
+        }
+      })
+    })
+  }
+
+  public switch(group: string, mode: boolean) {
+    let modeString = mode ? 'on' : 'off'
+    let options: Options = {
+      mode: 'text',
+      pythonPath: '/usr/bin/python3',
+      scriptPath: '/home/pi/Programming/Automation/executables',
+      args: [group, modeString, '--attempts', '3'],
+    }
+    if (mode) {
+      this.onOutlets.add(group)
+    } else {
+      this.onOutlets.delete(group)
+    }
+    eventEmitter.emit('groupChangedEvent' + group)
+    PythonShell.run('rfoutlets_switch_group.py', options, function(
+      err,
+      results
+    ) {
+      if (err) throw err
+      console.log()
+      console.log(`PythonShell:`)
+      console.log(`  results: ${results}`)
+      console.log(`  modeString: ${modeString}`)
+      console.log(`  group: ${group}`)
+      console.log()
+    })
+  }
+
+  public groupOn(group): boolean {
+    return this.onOutlets.has(group)
+  }
+
+  // public toggle(group: string): boolean {
+  //   let nextMode: boolean = !this.groupOn(group)
+  //   this.switch(group, nextMode)
+  //   return nextMode
+  // }
+
+  public emit(group: string, mode: boolean): void {
+    const returnData: SocketData = {
+      group: group,
+      value: mode ? 1 : 0,
+    }
+    // console.log(`emit\n  returnData: ${returnData.group}`)
+    this.io.emit(this.socketName, returnData)
+  }
+
+  public toggle(group: string): boolean {
+    let nextMode: boolean = !this.groupOn(group)
+    this.emit(group, nextMode)
+    this.switch(group, nextMode)
+    return nextMode
+  }
+
+  public watch(group: string, callback: { (test: string): void }): void {
+    eventEmitter.on('groupChangedEvent' + group, callback)
+  }
+}
---
+++ outlets-web-remote/webserver.ts
@@ -0,0 +1,67 @@
+import * as httpModule from 'http'
+const http = httpModule.createServer(handler)
+import * as fs from 'fs'
+
+import { Gpio } from 'onoff'
+const LED = new Gpio(6, 'out')
+// 'rising': only presses are handled
+const pushButton = new Gpio(19, 'in', 'rising', { debounceTimeout: 10 })
+
+import { Outlets } from './Outlets'
+
+var modes = {
+  livingroom: false,
+  office: false,
+  fan: false,
+}
+
+// PARKED ATTEMPT TO DO THINGS THE TYPESCRIPT WAY:
+// import { createServer } from 'http'
+// var http = createServer(handler)
+// import { fs } from 'fs'
+// import { socket } from 'socket.io'
+// var io = socket(http)
+// import { Gpio } from 'onoff'
+
+import { PythonShell } from 'python-shell'
+
+http.listen(8091)
+
+function handler(req, res) {
+  fs.readFile(__dirname + '/public/index.html', function(err, data) {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/html' })
+      return res.end('404 Not Found')
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.write(data)
+    return res.end()
+  })
+}
+
+let outlets = new Outlets(http, 'light')
+
+outlets.watch('officelight', test => {
+  LED.writeSync(outlets.groupOn('officelight') ? 1 : 0)
+})
+
+pushButton.watch(function(err, button_value) {
+  let mode: boolean
+  if (err) {
+    console.error(`There was an error ${err}`)
+    return
+  }
+  if (button_value === 1) {
+    mode = outlets.toggle('officelight')
+  } else {
+    // NOTE: pushButton set to 'rising'
+    console.log('WARNING: button release should not be processed')
+  }
+})
+
+process.on('SIGINT', function() {
+  LED.writeSync(0)
+  LED.unexport()
+  pushButton.unexport()
+  process.exit()
+})
